feed73729c67363710f1b58f9ff56828c980f80c
casrou (laptop) <casper.nc97@gmail.com>
2018-03-15 20:51:56 +0100

    Existing files

---
+++ DSBTickets/Controllers/HomeController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DSBTickets.Models;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace DSBTickets.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Journey> result = new List<Journey>();
+            Tuple<int, List<Journey>, bool, CookieContainer> t = getSearchId();
+            result.AddRange(t.Item2);
+
+            if (t.Item3)
+                result.AddRange(getJourneys(t.Item1, t.Item4));
+            result = result.OrderBy(j => j.lowestPrice).ToList();
+            return View(result);
+        }
+
+        //public static HashSet<Journey> getTickets()
+        //{
+        //    RestClient client = new RestClient("https://www.dsb.dk");
+        //    RestRequest req1 = new RestRequest("api/netbutik/search");
+        //    req1.Method = Method.POST;
+        //    req1.AddParameter("criteria[0][Direction]", 1)
+        //        .AddParameter("criteria[0][DepartLocation]", "Aarhus H")
+        //        .AddParameter("criteria[0][ArriveLocation]", "København H")
+        //        .AddParameter("criteria[0][SearchDate]", "2018-03-14")
+        //        .AddParameter("criteria[0][SearchType]", 7)
+        //        .AddParameter("criteria[0][PassengersAdults]", 1);
+
+        //    HashSet<Journey> journeys = new HashSet<Journey>();
+        //    for (int i = 12; i < 15; i++)
+        //    {
+        //        var c = client.Execute(req1);
+
+        //        var content1 = c.Content;
+
+        //        RootObject root = JsonConvert.DeserializeObject<RootObject>(content1.Substring(1, content1.Length - 2));
+
+        //        Console.WriteLine("TIME: " + i + ":00:00");
+        //        foreach (Journey j in root.journeys)
+        //        {
+        //            journeys.Add(j);
+        //        }
+        //    }
+        //    return journeys;
+        //}
+
+        public static Tuple<int, List<Journey>, bool, CookieContainer> getSearchId()
+        {
+            CookieContainer cc = new CookieContainer();
+            RestClient client = new RestClient("https://www.dsb.dk");
+            client.CookieContainer = cc;
+            RestRequest req1 = new RestRequest("api/netbutik/search");
+            req1.Method = Method.POST;
+            req1.AddParameter("criteria[0][Direction]", 1)
+                .AddParameter("criteria[0][DepartLocation]", "Aarhus H")
+                .AddParameter("criteria[0][ArriveLocation]", "København H")
+                .AddParameter("criteria[0][SearchDate]", "2018-04-1")
+                .AddParameter("criteria[0][SearchType]", 7)
+                .AddParameter("criteria[0][PassengersAdults]", 1)
+                .AddParameter("criteria[0][SearchTime]", "04:00:00");
+                        
+            var c = client.Execute(req1);
+            var content1 = c.Content;
+
+            RootObject root = JsonConvert.DeserializeObject<RootObject>(content1.Substring(1, content1.Length - 2));
+                            
+            return new Tuple<int, List<Journey>, bool, CookieContainer>(root.journeys[0].searchId, root.journeys, root.canSearchLater, cc);
+        }
+
+        public static List<Journey> getJourneys(int searchId, CookieContainer cc)
+        {
+            RestClient client = new RestClient("https://www.dsb.dk");
+            client.CookieContainer = cc;
+            RestRequest req1 = new RestRequest("api/netbutik/journeys/"+searchId+"/later");
+            req1.Method = Method.GET;
+
+            List<Journey> journeys = new List<Journey>();
+            RootObject root;
+            
+            do
+            {
+            var c = client.Execute(req1);
+
+            var content1 = c.Content;
+
+            root = JsonConvert.DeserializeObject<RootObject>(content1);
+
+            foreach (Journey j in root.journeys)
+            {
+                journeys.Add(j);
+            }                
+            } while (root.canSearchLater);
+            
+            return journeys;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DSBTickets/Models/Model.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DSBTickets.Models
+{
+    public class TransportType
+    {
+        //public int id { get; set; }
+        //public int transportTypeFamily { get; set; }
+        //public string iconCssClass { get; set; }
+    }
+
+    public class Stretch
+    {
+        //public int id { get; set; }
+        //public string lineName { get; set; }
+        //public TransportType transportType { get; set; }
+        //public string departLocation { get; set; }
+        //public DateTime departureDate { get; set; }
+        //public bool departureLocationIsInTS { get; set; }
+        //public string arriveLocation { get; set; }
+        //public DateTime arrivalDate { get; set; }
+        //public bool arrivalLocationIsInTS { get; set; }
+        //public bool isToFromBornholm { get; set; }
+        //public int status { get; set; }
+    }
+
+    public class StretchInfo
+    {
+        //public int __invalid_name__10 { get; set; }
+        //public int? __invalid_name__20 { get; set; }
+        //public int? __invalid_name__30 { get; set; }
+    }
+
+    public class TicketDetail
+    {
+        //public int type { get; set; }
+        //public string zones { get; set; }
+        //public string validInMinutes { get; set; }
+        //public bool immediatelyIsAvailable { get; set; }
+    }
+
+    public class TicketInstance
+    {
+        //public int journeyId { get; set; }
+        //public string code { get; set; }
+        //public string ticketName { get; set; }
+        //public string seatZoneName { get; set; }
+        //public int ticketClass { get; set; }
+        //public int sortIndex { get; set; }
+        //public bool isAvailable { get; set; }
+        //public bool isMainProduct { get; set; }
+        //public bool isVisible { get; set; }
+        //public double seatReservationPrice { get; set; }
+        //public double ticketPrice { get; set; }
+        //public string ticketCode { get; set; }
+        //public bool isSeatsSoldOut { get; set; }
+        //public bool isReservationRequired { get; set; }
+        //public bool hasTickets { get; set; }
+        //public bool hasSeatReservations { get; set; }
+        //public List<object> passengers { get; set; }
+        //public int seatReservations { get; set; }
+        //public string seatReservationCode { get; set; }
+        //public List<object> seatingGroups { get; set; }
+        //public List<object> productNotifications { get; set; }
+        //public StretchInfo stretchInfo { get; set; }
+        //public TicketDetail ticketDetail { get; set; }
+    }
+
+    public class Journey
+    {
+        //public List<Stretch> stretches { get; set; }
+        //public List<TicketInstance> ticketInstances { get; set; }
+        //public List<object> himMessages { get; set; }
+        //public int id { get; set; }
+        public int searchId { get; set; }
+        public double lowestPrice { get; set; }
+        //public int numberShifts { get; set; }
+        public DateTime arrivalDate { get; set; }
+        public DateTime departureDate { get; set; }
+        //public int status { get; set; }
+        //public int journeyTime { get; set; }
+        //public double ticketPrice { get; set; }
+        //public double seatReservationPrice { get; set; }
+        //public bool isAvailable { get; set; }
+        //public List<object> productNotifications { get; set; }
+
+        //public override bool Equals(object obj)
+        //{
+        //    Journey q = obj as Journey;
+        //    return q != null && q.departureDate == this.departureDate && q.arrivalDate == this.arrivalDate && q.lowestPrice == this.lowestPrice;
+        //}
+
+        //public override int GetHashCode()
+        //{
+        //    return this.departureDate.GetHashCode() ^ this.arrivalDate.GetHashCode() ^ this.lowestPrice.GetHashCode();
+        //}
+    }
+
+    public class RootObject
+    {
+        //public int direction { get; set; }
+        public List<Journey> journeys { get; set; }
+        //public bool canSearchEarlier { get; set; }
+        public bool canSearchLater { get; set; }
+        //public int searchType { get; set; }
+    }
+}
